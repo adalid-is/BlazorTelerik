288f96ff9c6a263ae5c02560a6955d6f620b3e75
adalid-is <aadalid@integrasoftware.biz>
2024-11-20 12:42:52 -0300

    first commit

---
+++ BlazorTelerik/Back/CategoryService.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using BlazorTelerik.Back;
+
+namespace BlazorTelerik.Back
+{
+    public class CategoryService
+    {
+        private HttpClient _http;
+
+        public CategoryService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public Task<IEnumerable<CategoryDto>> GetCategories()
+        {
+            return _http.GetFromJsonAsync<IEnumerable<CategoryDto>>("https://demos.telerik.com/blazor-ui-service/api/Category/GetCategories");
+        }
+    }
+}
